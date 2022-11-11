709f7051cfd3e00639a222c84e907a43bd050a18
Nishimura44tky <83286066+Nishimura44tky@users.noreply.github.com>
2022-11-11 09:12:59 +0900

    Initial Commit

---
+++ Map/MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapApp.swift
+//  Map
+//
+//  Created by 西村拓也 on 2022/11/11.
+//
+
+import SwiftUI
+
+@main
+struct MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
